2036e6f7d99d31decd888b6a17f0338e5dea84c4
Charles <boury.charles@gmail.com>
2021-12-11 16:03:54 +0100

    Rotate plateau at constant speed

--- Assets/Scripts/Tourniquet.cs
+++ Assets/Scripts/Tourniquet.cs
@@ -6,16 +6,13 @@ public class Tourniquet : MonoBehaviour
 {
     public float speed = 1f;
     Rigidbody rb;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        speed += Time.deltaTime;
         rb.AddTorque(0f, Time.deltaTime * speed, 0f);
     }
 }
