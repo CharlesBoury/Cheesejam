65e16191ebafc77fd7815df25b7952e39e2707da
Charles <boury.charles@gmail.com>
2021-12-11 17:50:21 +0100

    Fix cheese falling through plateau

--- Assets/Scripts/CheeseSpawner.cs
+++ Assets/Scripts/CheeseSpawner.cs
@@ -13,24 +13,31 @@ public class CheeseSpawner : MonoBehaviour
     [Range(0f, 0.5f)]
 	public float minDistanceFromEdge = 0.2f;
 	public bool regular = true;
+	public Transform height;
+
+	void OnEnable()
+	{
+		if (height == null)
+			height = transform;
+	}
 
 	void InstantiateRandomCheese(Vector3 pos) {
-        int cheeseIndex = UnityEngine.Random.Range(0, cheeseList.Count);
-		Instantiate(cheeseList[cheeseIndex], pos, Quaternion.Euler(0, UnityEngine.Random.Range(0, 180), 0));
+        int cheeseIndex = Random.Range(0, cheeseList.Count);
+		Instantiate(cheeseList[cheeseIndex], pos, Quaternion.Euler(0, Random.Range(0, 180), 0));
 	}
 	
 	void InstantiateRandomCheese(Vector3 pos, float angle) {
-        int cheeseIndex = UnityEngine.Random.Range(0, cheeseList.Count);
+        int cheeseIndex = Random.Range(0, cheeseList.Count);
 		Instantiate(cheeseList[cheeseIndex], pos, Quaternion.Euler(0, - angle * Mathf.Rad2Deg, 0));
 	}
 
 	void totalRandom() {
 		for (int i = 0; i < nbCheesesAtStart; i++) {		
-			float radius = UnityEngine.Random.Range(minDistanceFromCenter, (transform.localScale.x / 2) - minDistanceFromEdge);
+			float radius = Random.Range(minDistanceFromCenter, (transform.localScale.x / 2) - minDistanceFromEdge);
 			float angle = Random.Range(0f, 2f * Mathf.PI);
 			float x = Mathf.Cos(angle) * radius;
 			float z = Mathf.Sin(angle) * radius;
-			InstantiateRandomCheese(new Vector3(transform.position.x + x, transform.position.y + transform.localScale.y, transform.position.z + z));
+			InstantiateRandomCheese(new Vector3(height.position.x + x, height.position.y, height.position.z + z));
 		}
 	}
 
@@ -39,16 +46,16 @@ public class CheeseSpawner : MonoBehaviour
     {
 		float angle;
 
-		float regularRadius = UnityEngine.Random.Range(minDistanceFromCenter, (transform.localScale.x / 2) - minDistanceFromEdge);
+		float regularRadius = Random.Range(minDistanceFromCenter, (transform.localScale.x / 2) - minDistanceFromEdge);
         for (int i = 0; i < nbCheesesAtStart; i++) {		
-			float radius = UnityEngine.Random.Range(minDistanceFromCenter, (transform.localScale.x / 2) - minDistanceFromEdge);
+			float radius = Random.Range(minDistanceFromCenter, (transform.localScale.x / 2) - minDistanceFromEdge);
 			if (regular)
 				angle = (2 * i * Mathf.PI) / nbCheesesAtStart;
 			else
 				angle = Random.Range(0f, 2f * Mathf.PI);
 			float x = Mathf.Cos(angle) * radius;
 			float z = Mathf.Sin(angle) * radius;
-			InstantiateRandomCheese(new Vector3(transform.position.x + x, transform.position.y + transform.localScale.y, transform.position.z + z), angle);
+			InstantiateRandomCheese(new Vector3(height.position.x + x, height.position.y, height.position.z + z), angle);
 		}
     }
 
