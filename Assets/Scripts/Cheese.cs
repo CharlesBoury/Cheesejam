fdcc0ba8693fd723dd109bfa92f5d511b6e60ed0
ldedier <dedierludovic@gmail.com>
2021-12-11 11:41:44 +0100

    added Babybel as prefab variant of Cheese

--- Assets/Scripts/Cheese.cs
+++ Assets/Scripts/Cheese.cs
@@ -7,16 +7,4 @@ public class Cheese : MonoBehaviour
     public float hardness = 1f;
     public float density = 1f;
     public bool cutable = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
