2af968bcfb39954639eb17c239a8eda1d305b66e
Charles <boury.charles@gmail.com>
2021-12-12 16:29:14 +0100

    Tourniquééééé plus rapiiiiiide

--- Assets/Scripts/Tourniquet.cs
+++ Assets/Scripts/Tourniquet.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Tourniquet : MonoBehaviour
 {
-    public float speed = 1f;
-    Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	public float speed = 1f;
+	Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
-    void Update()
-    {
-        rb.AddTorque(0f, Time.deltaTime * speed, 0f);
-    }
+	void FixedUpdate()
+	{
+		int step = ((int)Time.time) / 10 + 1;
+		Vector3 deltaRotation = new Vector3(0, 1, 0) * Time.fixedDeltaTime * speed * step * step;
+
+		rb.angularVelocity = deltaRotation;
+	}
 }
